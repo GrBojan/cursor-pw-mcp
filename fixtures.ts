233371490159b426e349a7419b54f9d0663aa69a
bojan.grujicic <bojan.grujicic@rubylife.com>
2025-07-14 12:33:27 +0200

    chore: remove unused SignUp and Home POMs and step definitions, update fixtures accordingly

--- fixtures.ts
+++ fixtures.ts
@@ -1,13 +1,9 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: 'envVariables/.env' });
+dotenv.config();
 import { test as base } from '@playwright/test';
 import { LoginPage } from './src/pages/LoginPage';
-import { SignUpPage } from './src/pages/SignUpPage';
-import { HomePage } from './src/pages/HomePage';
-import { LoginSteps } from './src/stepDefinitions/LoginSteps';
-import { SignUpSteps } from './src/stepDefinitions/SignUpSteps';
-import { HomeSteps } from './src/stepDefinitions/HomeSteps';
 import { InventoryPage } from './src/pages/InventoryPage';
+import { LoginSteps } from './src/stepDefinitions/LoginSteps';
 import { InventorySteps } from './src/stepDefinitions/InventorySteps';
 import { CartPage } from './src/pages/CartPage';
 import { CartSteps } from './src/stepDefinitions/CartSteps';
@@ -18,12 +14,8 @@ import { BasePage } from './src/pages/BasePage';
 export const test = base.extend<{
   basePage: BasePage;
   loginPage: LoginPage;
-  signUpPage: SignUpPage;
-  homePage: HomePage;
-  loginSteps: LoginSteps;
-  signUpSteps: SignUpSteps;
-  homeSteps: HomeSteps;
   inventoryPage: InventoryPage;
+  loginSteps: LoginSteps;
   inventorySteps: InventorySteps;
   cartPage: CartPage;
   cartSteps: CartSteps;
@@ -38,29 +30,13 @@ export const test = base.extend<{
     const loginPage = new LoginPage(page);
     await use(loginPage);
   },
-  signUpPage: async ({ page }, use) => {
-    const signUpPage = new SignUpPage(page);
-    await use(signUpPage);
-  },
-  homePage: async ({ page }, use) => {
-    const homePage = new HomePage(page);
-    await use(homePage);
+  inventoryPage: async ({ page }, use) => {
+    const inventoryPage = new InventoryPage(page);
+    await use(inventoryPage);
   },
   loginSteps: async ({ page }, use) => {
     const loginSteps = new LoginSteps(page);
     await use(loginSteps);
-  },
-  signUpSteps: async ({ page }, use) => {
-    const signUpSteps = new SignUpSteps(page);
-    await use(signUpSteps);
-  },
-  homeSteps: async ({ page }, use) => {
-    const homeSteps = new HomeSteps(page);
-    await use(homeSteps);
-  },
-  inventoryPage: async ({ page }, use) => {
-    const inventoryPage = new InventoryPage(page);
-    await use(inventoryPage);
   },
   inventorySteps: async ({ page }, use) => {
     const inventorySteps = new InventorySteps(page);
