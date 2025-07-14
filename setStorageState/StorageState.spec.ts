19066c5c9f300ecbeb396cef9534a6348a7f5d9b
bojan.grujicic <bojan.grujicic@rubylife.com>
2025-07-14 13:41:37 +0200

    test: add test.step blocks to StorageState storage state test for better reporting

--- setStorageState/StorageState.spec.ts
+++ setStorageState/StorageState.spec.ts
@@ -5,8 +5,12 @@ import fs from 'fs';
 
 const storagePath = 'setStorageState/storageStateFiles/storageState.json';
 
-test('should login and save storage state', async ({ page }) => {
+test('should login and save storage state', async ({ page }, testInfo) => {
     const storage = new StorageState(page, storagePath);
-    await storage.loginAndSave(users.standard.username ?? '', users.standard.password ?? '', '/');
-    expect(fs.existsSync(storagePath)).toBeTruthy();
+    await test.step('Login and save storage state', async () => {
+        await storage.loginAndSave(users.standard.username ?? '', users.standard.password ?? '', '/');
+    });
+    await test.step('Check that storage state file exists', async () => {
+        expect(fs.existsSync(storagePath)).toBeTruthy();
+    });
 });
